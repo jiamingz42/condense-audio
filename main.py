789880299ba0d757bfea1512c0c19b2788d8fe3c
Jason Zhang <jason721b@gmail.com>
2021-03-14 21:52:11 -0700

    Set default value

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from subprocess import *
 from typing import NamedTuple
 from tqdm import tqdm
 from trim_movie.timestamp import Timestamp
+from glob import glob
 
 
 import argparse
@@ -106,22 +107,52 @@ def main():
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('--vin', required=True,
                         dest='video_in', help='Video infile')
-    parser.add_argument('--out', required=True, help='Outfile')
-    parser.add_argument('--sin', required=True,
-                        dest='sub_in', help='Subtitle infile')
-    parser.add_argument('--sout', required=True,
-                        dest='sub_out', help='Subtitle infile')
-    parser.add_argument('--tmpdir', default="tmp")
+    parser.add_argument('--out', help='Outfile')
+    parser.add_argument('--sin', dest='sub_in',
+                        help='Subtitle infile. If not present, infer from `video_in`. For example, if `video_in` is /path/to/foo.mp4, then this field will be /path/to/foo.vtt')
+    parser.add_argument('--sout', dest='sub_out', help='Subtitle outfile.')
+    parser.add_argument('--tmpdir', default="/tmp/lingq")
     parser.add_argument('--keep-tmpdir', default=False, action='store_true')
 
     args = parser.parse_args()
 
     tmpdir = args.tmpdir
     keep_tmpdir = args.keep_tmpdir
-    subtitle_infile = os.path.abspath(args.sub_in)
-    subtitle_outfile = os.path.abspath(args.sub_out)
     video_infile = os.path.abspath(args.video_in)
-    final_outfile = os.path.abspath(args.out)
+
+    if args.sub_in:
+        subtitle_infile = os.path.abspath(args.sub_in)
+    else:
+        # Infer from `video_infile`
+        # For example, `video_infile` = "雙層公寓：東京 2019-2020_S01E01_重返東京.mp4"
+        #                                                       ******
+        #  we will try to find subtitle in the same directory matching `*S01E01*.vtt`
+        # TODO: Extract to a helper method (video_infile -> subtitle_infile)
+        match = re.match(".*(S\d+E\d+)", video_infile)
+        assert match is not None, "Did not specify `--sin`. Can't infer from `--vin` either."
+        pattern = os.path.join(os.path.dirname(
+            video_infile), "*{pattern}*.vtt".format(pattern=match.group(1)))
+        file_matches = glob(pattern)
+        assert len(
+            file_matches) == 1, "len(file_matches) is not 1. file_matches = %s" % file_matches
+        subtitle_infile = file_matches[0]
+
+    if args.sub_out:
+        subtitle_outfile = os.path.abspath(args.sub_out)
+    else:
+        match = re.match(".*(S\d+E\d+)", video_infile)
+        assert match is not None, "Did not specify `--sin`. Can't infer from `--sout` either."
+        subtitle_filename = '{idx}.vtt'.format(idx=match.group(1))
+        subtitle_outfile = os.path.join(os.path.dirname(video_infile), 'condensed', subtitle_filename)
+
+    if args.sub_out:
+        final_outfile = os.path.abspath(args.out)
+    else:
+        match = re.match(".*(S\d+E\d+)", video_infile)
+        assert match is not None, "Did not specify `--sin`. Can't infer from `--sout` either."
+        final_outfile_name = '{idx}.mp3'.format(idx=match.group(1))
+        final_outfile = os.path.join(os.path.dirname(video_infile), 'condensed', final_outfile_name)
+
     final_outfile_dir = os.path.dirname(final_outfile)
     list_file_path = os.path.join(tmpdir, "list.txt")
 
@@ -133,6 +164,14 @@ def main():
     # Make sure the dir for outfile exists
     if not os.path.exists(final_outfile_dir):
         os.makedirs(final_outfile_dir)
+
+    print("Running with the following parameters:\n" +
+          'Input\n' +
+          '  Video = "%s"\n' % video_infile +
+          '  Sub = "%s"\n' % subtitle_infile +
+          'Output\n' +
+          '  Audio = "%s"\n' % final_outfile +
+          '  Sub = "%s"\n' % subtitle_outfile)
 
     outfiles = []  # will be mutated
     try:
@@ -212,6 +251,5 @@ def create_condense_audio(tmpdir, subtitle_infile, subtitle_outfile, video_infil
           (outfile_duration / video_in_duration * 100))
 
 
-
 if __name__ == '__main__':
-  main()
+    main()
