88a2831694b6709f5c05fa99902ef2ab449e8de5
Jason Zhang <jason721b@gmail.com>
2021-03-16 17:00:32 -0700

    Implement read_ass

--- trim_movie/subtitle.py
+++ trim_movie/subtitle.py
@@ -1,8 +1,8 @@
 from trim_movie.timestamp import Timestamp
-from typing import NamedTuple, List, Callable
+from typing import NamedTuple, List, Callable, Iterator, Any
 
+import ass
 import webvtt
-
 
 class Caption(NamedTuple):
     start: Timestamp
@@ -10,23 +10,35 @@ class Caption(NamedTuple):
     text: str
 
 
+# TODO: map_subtitle -> Generic type?
 def load_captions(subtitle_infile: str,
-                  is_valid_subtitle: Callable[[webvtt.Caption], bool],
-                  map_subtile: Callable[[webvtt.Caption], webvtt.Caption]) -> List[Caption]:
+                  is_valid_subtitle: Callable[[Any], bool],
+                  map_subtile: Callable[[Any], Any]) -> List[Caption]:
     if subtitle_infile.endswith(".vtt"):
         return [*map(map_subtile, filter(is_valid_subtitle, read_webvtt(subtitle_infile)))]
     elif subtitle_infile.endswith(".ass"):
-        raise NotImplemented
+        return [*read_ass(subtitle_infile)]
     else:
         raise ValueError("Unsupported subtitle type: %s" % subtitle_infile)
 
 
-def read_webvtt(infile: str):
+def read_webvtt(infile: str) -> Iterator[Caption]:
     for caption in webvtt.read(infile):
         yield Caption(
             Timestamp.from_s(caption.start),
             Timestamp.from_s(caption.end),
             caption.text
+        )
+
+def read_ass(infile: str) -> Iterator[Caption]:
+    with open(infile, "r") as f:
+        ass_subtitle = ass.parse(f)
+    for event in ass_subtitle.events:
+        # TODO: Can filter by style
+        yield Caption(
+            Timestamp.from_timedelta(event.start),
+            Timestamp.from_timedelta(event.end),
+            event.text
         )
 
 
