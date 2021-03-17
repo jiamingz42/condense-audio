2e4bd794513f6055721b9a6836594fdba979787e
Jason Zhang <jason721b@gmail.com>
2021-03-17 12:49:17 -0700

    Update tests/test_timestamp.py to use unittest module

--- tests/test_timestamp.py
+++ tests/test_timestamp.py
@@ -1,12 +1,18 @@
 from trim_movie.timestamp import Timestamp
 
-def test_add():
-    assert Timestamp.from_s(
-        "00:01:00.000") + Timestamp.from_s("00:00:30.000") == Timestamp.from_s("00:01:30.000")
+import unittest
 
-def test_minus():
-    assert Timestamp.from_s(
-        "00:01:00.000") - Timestamp.from_s("00:00:30.000") == Timestamp.from_s("00:00:30.000")
+class TestTimeStamp(unittest.TestCase):
+    def test_add(self):
+        assert Timestamp.from_s(
+            "00:01:00.000") + Timestamp.from_s("00:00:30.000") == Timestamp.from_s("00:01:30.000")
 
-def test_from_s():
-    assert Timestamp.from_s("00:04:28.351").total_milliseconds == 4 * 60000 + 28000 + 351
+    def test_minus(self):
+        assert Timestamp.from_s(
+            "00:01:00.000") - Timestamp.from_s("00:00:30.000") == Timestamp.from_s("00:00:30.000")
+
+    def test_from_s(self):
+        assert Timestamp.from_s("00:04:28.351").total_milliseconds == 4 * 60000 + 28000 + 351
+
+if __name__ == '__main__':
+    unittest.main()
