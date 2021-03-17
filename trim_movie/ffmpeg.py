8c31c0a1274418b18e12478a37abff35e17d45bb
Jason Zhang <jason721b@gmail.com>
2021-03-16 21:34:31 -0700

    Add type hinting to ffmpeg.py

--- trim_movie/ffmpeg.py
+++ trim_movie/ffmpeg.py
@@ -3,20 +3,25 @@ Python wrapper for cli `ffmpeg` command
 """
 
 from subprocess import *
+from typing import Iterable
 
-def run_command(cmd):
+import shlex
+
+def run_command(cmd: str) -> None:
     with Popen(shlex.split(cmd), stdout=PIPE) as proc:
+        if not proc.stdout:
+            return
         stdout = proc.stdout.read()
         if stdout:
             print(stdout)
 
 
-def cut_out_video(infile, outfile, start_time, duration):
+def cut_out_video(infile: str, outfile: str, start_time, duration) -> None:
     cmd = f"ffmpeg -hide_banner -loglevel error -i '{infile}' -ss {start_time} -t {duration} -c:a copy -map 0:1 -y '{outfile}'"
     run_command(cmd)
 
 
-def concat_video(infile, outfile):
+def concat_video(infile: str, outfile: str) -> None:
     cmd = f"ffmpeg -hide_banner -loglevel error -safe 0 -f concat -segment_time_metadata 1 -i {infile} -vf select=concatdec_select -af aselect=concatdec_select,aresample=async=1 -y '{outfile}'"
     with Popen(shlex.split(cmd), stdout=PIPE, stderr=STDOUT) as proc:
         for i, line in enumerate(readlines(proc.stdout)):
@@ -24,7 +29,7 @@ def concat_video(infile, outfile):
             continue
 
 
-def readlines(stream):
+def readlines(stream) -> Iterable[str]:
     while True:
         line = stream.readline().decode('utf-8')
         if line:
@@ -33,10 +38,12 @@ def readlines(stream):
             break
 
 
-def get_duration(infile):
+def get_duration(infile: str) -> float:
     cmd = f"ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 '{infile}'"
     with Popen(shlex.split(cmd), stdout=PIPE, stderr=STDOUT) as proc:
+        if not proc.stdout:
+            return 0
         stdout = proc.stdout.read().decode("utf-8").strip()
         if not stdout:
-            raise StandardError()
+            raise ValueError("No stdout")
         return float(stdout)
