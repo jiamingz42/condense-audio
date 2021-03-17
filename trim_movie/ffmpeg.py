77f704d6a9314d132513b0fa42e79c94791d896a
Jason Zhang <jason721b@gmail.com>
2021-03-17 11:15:13 -0700

    Add method _readlines_with_idx; mark private method in ffmpeg with prefix _

--- trim_movie/ffmpeg.py
+++ trim_movie/ffmpeg.py
@@ -3,39 +3,25 @@ Python wrapper for cli `ffmpeg` command
 """
 
 from subprocess import *
-from typing import Iterable
+from typing import Iterable, Tuple
 
 import shlex
-
-def run_command(cmd: str) -> None:
-    with Popen(shlex.split(cmd), stdout=PIPE) as proc:
-        if not proc.stdout:
-            return
-        stdout = proc.stdout.read()
-        if stdout:
-            print(stdout)
 
 
 def cut_out_video(infile: str, outfile: str, start_time, duration) -> None:
     cmd = f"ffmpeg -hide_banner -loglevel error -i '{infile}' -ss {start_time} -t {duration} -c:a copy -map 0:1 -y '{outfile}'"
-    run_command(cmd)
+    _run_command(cmd)
 
 
-def concat_video(infile: str, outfile: str) -> None:
-    cmd = f"ffmpeg -hide_banner -loglevel error -safe 0 -f concat -segment_time_metadata 1 -i {infile} -vf select=concatdec_select -af aselect=concatdec_select,aresample=async=1 -y '{outfile}'"
+def concat_video(infile: str, outfile: str, files_count: int) -> None:
+    cmd = ("ffmpeg -hide_banner -loglevel debug -safe 0 -f concat " +
+           f"-segment_time_metadata 1 -i {infile} " +
+           "-vf select=concatdec_select " +
+           f"-af aselect=concatdec_select,aresample=async=1 -y '{outfile}'")
     with Popen(shlex.split(cmd), stdout=PIPE, stderr=STDOUT) as proc:
-        for i, line in enumerate(readlines(proc.stdout)):
-            #print(i, line[:-1])
+        for idx, line in _readlines_with_idx(proc.stdout):
+            print(f'>{idx:03d}< {line}')
             continue
-
-
-def readlines(stream) -> Iterable[str]:
-    while True:
-        line = stream.readline().decode('utf-8')
-        if line:
-            yield line
-        else:
-            break
 
 
 def get_duration(infile: str) -> float:
@@ -47,3 +33,26 @@ def get_duration(infile: str) -> float:
         if not stdout:
             raise ValueError("No stdout")
         return float(stdout)
+
+
+def _readlines_with_idx(stream) -> Iterable[Tuple[int, str]]:
+    for idx, line in enumerate(readlines(stream)):
+        yield (idx, line)
+
+
+def _run_command(cmd: str) -> None:
+    with Popen(shlex.split(cmd), stdout=PIPE) as proc:
+        if not proc.stdout:
+            return
+        stdout = proc.stdout.read()
+        if stdout:
+            print(stdout)
+
+
+def _readlines(stream) -> Iterable[str]:
+    while True:
+        line = stream.readline().decode('utf-8')
+        if line:
+            yield line.strip()
+        else:
+            break
