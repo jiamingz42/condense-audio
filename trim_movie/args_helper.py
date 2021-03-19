f11b73a22c118b63572a3471e7841b5d5b30feda
Jason Zhang <jason721b@gmail.com>
2021-03-19 07:37:16 -0700

    Fix a issue in args_helper.py

--- trim_movie/args_helper.py
+++ trim_movie/args_helper.py
@@ -1,4 +1,4 @@
-from tying import Union, Match
+from typing import Union, Match
 
 import os
 import sys
@@ -9,7 +9,7 @@ def get_subtitle_outfile(origin_sub_out: str, match: Union[Match, None], video_i
         return os.path.abspath(origin_sub_out)
     # TODO: Check `match.groups()` has at least one item
     elif match:
-        subtitle_filename = '{idx}.vtt'.format(idx=match.group(1).strip())
+        subtitle_filename = '{idx}.vtt'.format(idx=_get_output_idx(match))
         return os.path.join(os.path.dirname(video_infile), 'condensed', subtitle_filename)
     else:
         raise ValueError("Did not specify input subtitle `--sin`. Can't infer from input video `--vin` either.")
@@ -19,7 +19,11 @@ def get_audio_outfile(origin_audio_out: str, match: Union[Match, None], video_in
    if origin_audio_out:
         return os.path.abspath(origin_audio_out)
    elif match:
-       final_outfile_name = '{idx}.mp3'.format(idx=match.group(1))
+       final_outfile_name = '{idx}.mp3'.format(idx=_get_output_idx(match))
        return os.path.join(os.path.dirname(video_infile), 'condensed', final_outfile_name)
    else:
        raise ValueError("Did not specify output audio `--out`. Can't infer from input video `--vin` either.")
+
+
+def _get_output_idx(match: Match) -> str:
+    return match.group(1).strip()
